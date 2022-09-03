6ea1dd2043c61ed457a70c8404c74f25940cec89
noman-swe <noman.swe130@gmail.com>
2022-09-04 01:09:06 +0600

    34-7 Create dynamic url based on click and display data

--- API-Examples/MealDB/js/meal.js
+++ API-Examples/MealDB/js/meal.js
@@ -14,10 +14,11 @@ const searchFood = () => {
 const displaySearchResult = (meals) => {
     const searchResult = document.getElementById('search-result');
     meals.forEach(meal => {
+
         const divCol = document.createElement('div');
         divCol.classList.add('col');
         divCol.innerHTML = `
-                <div class="card shadow p-3 mb-5 bg-white rounded">
+                <div onclick="loadMealDetails(${meal.idMeal})" class="card shadow p-3 mb-5 bg-white rounded">
                     <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">${meal.strMeal}</h5>
@@ -29,3 +30,29 @@ const displaySearchResult = (meals) => {
     });
 
 }
+
+const loadMealDetails = mealId => {
+    // console.log(mealId);
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetails(data.meals[0]));
+}
+
+const displayMealDetails = (meal) => {
+    const displayMealDetailsSection = document.getElementById('display-meal-details');
+    const showDetailDiv = document.createElement('div');
+    showDetailDiv.innerHTML = `
+            <div class="card text-bg-dark shadow p-3 mb-5 bg-white rounded">
+                <img src="${meal.strMealThumb}" class="card-img details-img" alt="...">
+                <div class="card-img-overlay">
+                    <h5 class="card-title text-center text-danger pt-5 details-title">${meal.strMeal}</h5>
+                    <p class="card-text px-3">${meal.strInstructions.slice()}</p>
+                </div>
+                </div>
+                <h3 class='pt-3 text-success text-center'> Related Foods...</h3>
+        `;
+
+    displayMealDetailsSection.appendChild(showDetailDiv);
+}
+
